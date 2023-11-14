f16eb53a4ee7c65c1fd189cb0c6d7e280729bc7c
Luka1321 <luka.bombek123@gmail.com>
2023-11-14 10:40:30 +0100

    Function for validating block

---
+++ News_Blockchain/BlockValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News_Blockchain
+{
+    class BlockValidator
+    {
+        public bool ValidateBlock(Block block)
+        {
+            return true;
+        }
+    }
+}
