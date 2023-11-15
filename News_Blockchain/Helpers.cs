fa756c32ca1da67e72cb1ec4b40c1a93c0b444e5
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-11-15 20:21:54 +0100

    Adding helper class and hashing function

--- News_Blockchain/Helpers.cs
+++ News_Blockchain/Helpers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text;
 using System.Text.Json;
+using System.Security.Cryptography;
 
 namespace News_Blockchain
 {
@@ -44,6 +46,34 @@ namespace News_Blockchain
 		{
             return JsonSerializer.Deserialize<Transaction>(transaction);
 
+        }
+    }
+
+    public static class Helpers
+    {
+        /// <summary>
+        /// Every input of the function is hashed twice via SHA256. 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="timesHashed"></param>
+        /// <returns>SHA256 hash</returns>
+        public static string ComputeSHA256Hash(string input, int timesHashed = 1)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                if (timesHashed == 2)
+                    return stringBuilder.ToString();
+                return ComputeSHA256Hash(stringBuilder.ToString(), 2);
+            }
         }
     }
 }
