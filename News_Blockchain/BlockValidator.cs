824af36fb0e1d3350d65b777fd84c08e11b17a8a
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-11-18 15:20:48 +0100

    Adding CheckHashDifficultyTarget validator

--- News_Blockchain/BlockValidator.cs
+++ News_Blockchain/BlockValidator.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace News_Blockchain
 {
     class BlockValidator
     {
+        /// <summary>
+        /// Function checks given block for any potential rule violations and marks it as
+        /// valid in case of no violations.
+        /// </summary>
+        /// <param name="block"></param>
+        /// <returns>true or false</returns>
         public bool ValidateBlock(Block block)
         {
             return true;
@@ -55,6 +62,27 @@ namespace News_Blockchain
                         InternalNodes[height - 1][index] + InternalNodes[height - 1][index + 1]));
                 }
             }
+        }
+
+        /// <summary>
+        /// Functions checks if provided hash satisfies nBits requirement.
+        /// It is not intended to be used for mining, because target is calculated every time.
+        /// </summary>
+        /// <param name="headerHash"></param>
+        /// <param name="nBits"></param>
+        /// <returns>true or false</returns>
+        private bool CheckHashDifficultyTarget(string headerHash, int nBits)
+        {
+            int significand = int.Parse(nBits.ToString("X").Substring(2), System.Globalization.NumberStyles.HexNumber);
+            int exponent = int.Parse(nBits.ToString("X").Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            BigInteger target = significand * BigInteger.Pow(256, exponent - 3);
+
+            BigInteger HexHashValue = BigInteger.Parse(headerHash, System.Globalization.NumberStyles.HexNumber);
+
+            if (HexHashValue > target)
+                return false;
+
+            return true;
         }
     }
 }
