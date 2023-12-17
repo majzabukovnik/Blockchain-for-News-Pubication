40bb7cbf40dae2f23ae7664bceae415149d213bd
VegasCool <aney.vaishnav5@gmail.com>
2023-12-17 17:06:08 +0100

    UTEx class and more needed changes

--- News_Blockchain/Database.cs
+++ News_Blockchain/Database.cs
@@ -3,6 +3,11 @@ using Tenray.ZoneTree.Core;
 
 namespace News_Blockchain;
 
+public enum DB
+{
+    Blockchain,
+    UTEx
+}
 public class Database
 {
     /// <summary>
@@ -28,22 +33,32 @@ public class Database
     /// <summary>
     /// Creates or Opens a new connection to the LocalDB with custom path name
     /// </summary>
-    /// <param name="path">custom path name</param>
-    public Database(string path)
+    /// <param name="db">custom path name</param>
+    public Database(DB db)
     {
+        string path = db == DB.Blockchain ? "Blockchain" : "UTEx";
         _zoneTree = new ZoneTreeFactory<string, string>()
             .SetDataDirectory(path)
             .OpenOrCreate();
     }
     
     /// <summary>
-    /// Insert a new key-value pair (hash, block)
+    /// Insert a new key-value (hash, block)
     /// </summary>
     /// <param name="key">Key (Hash of the Block)</param>
     /// <param name="value">Value (Serialized Block)</param>
     public void InsertNewRecord(Block block)
     {
         _zoneTree.AtomicUpsert(Helpers.GetBlockHash(block), Serializator.SerializeToString(block));
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="trans"></param>
+    public void InsertNewRecord(UTExTrans trans)
+    {
+        _zoneTree.AtomicUpsert(trans.GetKey(), trans.GetValue());
     }
     
     /// <summary>
@@ -59,6 +74,29 @@ public class Database
     }
 
     /// <summary>
+    /// Deletes all records (Blocks from a specified key)
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if successful, false if not</returns>
+    public bool DeleteRecords(string key)
+    {
+        try
+        {
+            var iterator = _zoneTree.CreateIterator();
+            iterator.Seek(key);
+            while (iterator.Next())
+            {
+                _zoneTree.ForceDelete(iterator.CurrentKey);
+            }
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+    
+    /// <summary>
     /// Get all records store in the database
     /// </summary>
     /// <returns>All records</returns>
@@ -72,7 +110,6 @@ public class Database
         }
 
         return DB;
-        //Nevem ce to rabmo... kommentiri na to
     }
     
     /// <summary>
@@ -82,5 +119,47 @@ public class Database
     public int GetAllRecordsCount()
     {
         return (int)_zoneTree.Count();
+    }
+}
+
+public class UTExTrans
+{
+    private string _hashTrans;
+    private int _index;
+    private string _hashBlock;
+    
+    public string HashTrans
+    {
+        get { return _hashTrans; }
+        set { _hashTrans = value; }
+    }
+
+    public int Index
+    {
+        get { return _index; }
+        set { _index = value; }
+    }
+    
+    public string HashBlock
+    {
+        get { return _hashBlock; }
+        set { _hashBlock = value; }
+    }
+
+    public UTExTrans(string hashTrans, int index, string hashBlock)
+    {
+        _hashTrans = hashTrans;
+        _index = index;
+        _hashBlock = hashBlock;
+    }
+
+    public string GetKey()
+    {
+        return _hashTrans + "-" + _index;
+    }
+
+    public string GetValue()
+    {
+        return _hashBlock;
     }
 }
