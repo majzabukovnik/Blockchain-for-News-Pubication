dda8996cbae80349939cc389850a6351904d14af
Luka1321 <luka.bombek123@gmail.com>
2024-02-26 18:54:08 +0100

    saving unfinished changes

--- News_Blockchain/Frontend/script.js
+++ News_Blockchain/Frontend/script.js
@@ -1,8 +1,13 @@
+/*var feesRange = document.getElementById("fees");
+var feeOutput = document.getElementById("fee-output");
+
+feesRange.addEventListener("input", function() {   
+    feeOutput.textContent = feesRange.value;
+});
+*/
 var feesRange = document.getElementById("fees");
 var feeOutput = document.getElementById("fee-output");
 
-// Add an input event listener to the range input
-feesRange.addEventListener("input", function() {
-    // Update the value of the output element with the current value of the range input
-    feeOutput.textContent = feesRange.value;
+feesRange.addEventListener("input", function() {   
+    feeOutput.textContent = feesRange.value + " satoshi/byte";
 });
